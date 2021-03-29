55629700fd47d59925c8e65dfbd91fe92533e1d2
naaem <naaem@gmail.com>
2021-03-29 14:27:41 +0530

    Implemented a method to check equivalancy of 1 dollar and 75.85 rupees

--- src/main/java/utility/Wallet.java
+++ src/main/java/utility/Wallet.java
@@ -1,4 +1,23 @@
 package utility;
 
 public class Wallet {
+    double dollars;
+    double rupees;
+    double dollarValueInRupees = 74.85;
+    public Wallet(double dollars, double rupees) {
+        this.dollars = dollars;
+        this.rupees = rupees;
+    }
+
+
+    public boolean checkIfXDollarsIsEqualToYRupees() {
+        if(convertDollarsToRupees() == rupees){
+            return true;
+        }
+        return false;
+    }
+
+    private double convertDollarsToRupees() {
+        return dollars*dollarValueInRupees;
+    }
 }
--- src/test/java/utility/WalletTest.java
+++ src/test/java/utility/WalletTest.java
@@ -6,4 +6,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WalletTest {
+    @Test
+    void TestIf1DollarEqualToNRupees() {
+        double dollars = 1;
+        double rupees = 74.85;
+        Wallet wallet = new Wallet(dollars,rupees);
+
+        boolean actualValue = wallet.checkIfXDollarsIsEqualToYRupees();
+
+        assertEquals(true,actualValue);
     }
+
+    @Test
+    void TestIf1DollarIsNotEqualTo75Dollars() {
+        double dollars = 1;
+        double rupees = 75;
+        Wallet wallet = new Wallet(dollars,rupees);
+
+        boolean actualValue = wallet.checkIfXDollarsIsEqualToYRupees();
+
+        assertEquals(false,actualValue);
+    }
+
+}
