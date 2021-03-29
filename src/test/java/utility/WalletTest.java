231ffa289a0e8cb0d6534c50a213e91b20daacf1
naaem <naaem@gmail.com>
2021-03-29 14:15:51 +0530

    environment setup for Wallet

---
+++ src/test/java/utility/WalletTest.java
@@ -0,0 +1,9 @@
+package utility;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WalletTest {
+    }
