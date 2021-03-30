141b405fe13f0fcb817a1e37f408e67f834eeb75
preethi <preethimandipalli99@gmail.com>
2021-03-30 16:13:02 +0530

    Added Functionality for finding the total amount in preferred currency

--- src/main/java/utility/Wallet.java
+++ src/main/java/utility/Wallet.java
@@ -3,7 +3,7 @@ package utility;
 public class Wallet {
 
     double dollarValueInRupees = 74.85;
-
+    double totalBalanceInRupees;
 
     public boolean checkIfXDollarsIsEqualToYRupees(Currency dollar,Currency rupee) {
         if(rupee.currencyValue == (convertDollarsToRupees(dollar.currencyValue))){
@@ -15,4 +15,29 @@ public class Wallet {
     private double convertDollarsToRupees(double dollars) {
         return dollars * dollarValueInRupees;
     }
+
+    private double convertRupeesToDollars(double currencyValue) {
+        return currencyValue/dollarValueInRupees;
+    }
+
+    public void addCurrencyToWallet(Currency currency) {
+        if(currency.currencyType.equals("rupees")){
+            totalBalanceInRupees += currency.currencyValue;
+        }
+        else{
+            totalBalanceInRupees += convertDollarsToRupees(currency.currencyValue);
+        }
+
+    }
+    public double getTotalAmountInPreferredCurrency(String preferredCurrency) {
+        if(preferredCurrency.equals("rupees")){
+
+            return Double.parseDouble(String.format("%.2f", totalBalanceInRupees));
+
+        }
+        return Double.parseDouble(String.format("%.2f", convertRupeesToDollars(totalBalanceInRupees)));
+
+    }
+
+
 }
--- src/test/java/utility/WalletTest.java
+++ src/test/java/utility/WalletTest.java
@@ -1,8 +1,5 @@
 package utility;
-
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WalletTest {
@@ -11,7 +8,7 @@ public class WalletTest {
         Currency rupee = new Currency(1,"rupees");
         Currency dollar = new Currency(74.85,"dollars");
         Wallet wallet = new Wallet();
-        
+
         boolean actualValue = wallet.checkIfXDollarsIsEqualToYRupees(rupee,dollar);
 
         assertEquals(true,actualValue);
@@ -19,13 +16,45 @@ public class WalletTest {
 
     @Test
     void TestIf1DollarNotEqualToNRupees() {
-        Currency rupee = new Currency(1,"rupees");
-        Currency dollar = new Currency(80,"dollars");
+        Currency rupee = new Currency(80,"rupees");
+        Currency dollar = new Currency(1,"dollars");
         Wallet wallet = new Wallet();
 
         boolean actualValue = wallet.checkIfXDollarsIsEqualToYRupees(rupee,dollar);
 
         assertEquals(false,actualValue);
+    }
+
+    @Test
+    void TestIfTotalAmountIsEqualToYRupees(){
+        Currency rupee = new Currency(50,"rupees");
+        Currency dollar = new Currency(1,"dollars");
+        String preferredCurrency = "rupees";
+        double expectedValue = 124.85;
+        Wallet wallet = new Wallet();
+
+        wallet.addCurrencyToWallet(rupee);
+        wallet.addCurrencyToWallet(dollar);
+        double actualValue = wallet.getTotalAmountInPreferredCurrency(preferredCurrency);
+
+        assertEquals(expectedValue,actualValue);
+
+    }
+
+    @Test
+    void TestIfTotalAmountIsEqualToYDollars(){
+        Currency rupee = new Currency(74,"rupees");
+        Currency dollar = new Currency(1,"dollars");
+        String preferredCurrency = "dollars";
+        double expectedAmount = 1.99;
+        Wallet wallet = new Wallet();
+
+        wallet.addCurrencyToWallet(rupee);
+        wallet.addCurrencyToWallet(dollar);
+        double actualAmount = wallet.getTotalAmountInPreferredCurrency(preferredCurrency);
+
+        assertEquals(expectedAmount,actualAmount);
+
     }
 
 
